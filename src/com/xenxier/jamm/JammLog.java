f6cd99ff8e169c4f0505e80b0361c7672fe58b37
Benjamin Gwynn <xenxier@gmail.com>
2014-04-18 22:44:57 +0100

    general: Start JAMM and commit code to Git.

---
+++ src/com/xenxier/jamm/JammLog.java
@@ -0,0 +1,28 @@
+package com.xenxier.jamm;
+
+import org.apache.logging.log4j.Level;
+import cpw.mods.fml.common.FMLLog;
+
+public class JammLog {
+    public static void log(char level, Object object) {
+    	if ( level == 'F') {
+    		fmlLog(Level.FATAL, object);
+    	} else if ( level == 'E' ) {
+    		fmlLog(Level.FATAL, object);
+    	} else if ( level == 'W' ) {
+    		fmlLog(Level.WARN, object);
+    	} else if ( level == 'I' ) {
+    		fmlLog(Level.INFO, object);
+    	} else if ( level == 'D' ) {
+    		fmlLog(Level.DEBUG, object);
+    	} else if ( level == 'T' ) {
+    		fmlLog(Level.TRACE, object);
+    	} else {  // If unknown then assume info
+    		fmlLog(Level.INFO, object);
+    	}
+    }
+    
+    public static void fmlLog(Level logLevel, Object object) {
+		FMLLog.log(JammMain.MODID, logLevel, String.valueOf(object));
+    }
+}
