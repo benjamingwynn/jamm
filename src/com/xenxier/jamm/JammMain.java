cd9edf0feb787458910d0a7f4a7230118d4ebea1
Benjamin Gwynn <xenxier@gmail.com>
2014-04-18 22:48:26 +0100

    cleanup: Fix whitespace issue.

--- src/com/xenxier/jamm/JammMain.java
+++ src/com/xenxier/jamm/JammMain.java
@@ -7,21 +7,21 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 @Mod(modid = JammMain.MODID, version = JammMain.VERSION)
 
 public class JammMain {
-    public static final String MODID = "JAMM";
-    public static final String VERSION = "0.1";
-    
-    @EventHandler
-    public void init(FMLInitializationEvent event) {
-    	startLoad();
-    	endLoad();
-    }
-    
+	public static final String MODID = "JAMM";
+	public static final String VERSION = "0.1";
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		startLoad();
+		endLoad();
+	}
+	
 	void startLoad() {
-    	JammLog.log('I', "Loading JAMM...");
+		JammLog.log('I', "Loading JAMM...");
 	}
 	
 	void endLoad() {
-    	JammLog.log('I', "Just Another Minecraft Mod - Version " + VERSION);
-    	JammLog.log('I', "Created by Benjamin Gwynn (Xenxier) - http://xenxier.tk");
+		JammLog.log('I', "Just Another Minecraft Mod - Version " + VERSION);
+		JammLog.log('I', "Created by Benjamin Gwynn (Xenxier) - http://xenxier.tk");
 	}
 }
