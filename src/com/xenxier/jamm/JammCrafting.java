0d0dc5e0a4d6a056dbdc9c4414bc4252415741e9
Benjamin Gwynn <xenxier@gmail.com>
2014-04-20 21:55:33 +0100

    crafting: Add recipe for Dried Wood and Compressed Wood.

--- src/com/xenxier/jamm/JammCrafting.java
+++ src/com/xenxier/jamm/JammCrafting.java
@@ -8,12 +8,25 @@ import net.minecraft.item.ItemStack;
 public class JammCrafting {
 
 	public static void loadCraftingRecipes() {
+		
 		JammLog.log('I', "Registering Crafting Recipes...");
+		
 		GameRegistry.addRecipe(new ItemStack(JammBlocks.damp_wood, 8), new Object[] {
 	    	"AAA",
 	    	"ABA",
 	    	"AAA",
 	    	'A', Blocks.planks, 'B', Items.water_bucket });
+		
+		GameRegistry.addRecipe(new ItemStack(JammBlocks.compressed_wood, 1), new Object[] {
+	    	"AAA",
+	    	"AAA",
+	    	"AAA",
+	    	'A', JammBlocks.dried_wood });
+		
+		GameRegistry.addRecipe(new ItemStack(JammBlocks.stone_wood, 3), new Object[] {
+	    	"ABA",
+	    	"ABA",
+	    	"ABA",
+	    	'A', JammBlocks.compressed_wood, 'B', Blocks.stone });
 	}
-
 }
